0c37249c3d4cf9498c7df32562e50542056dca88
Riyazsuhu <riyazsuhu1999@gmail.com>
2020-05-13 14:09:01 +0530

    Geocode error handled

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,14 +2,15 @@ const request=require('request')
 const geocode=(address,callback)=>{
     let url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoicml5YXoxOTk5IiwiYSI6ImNrOHN1bW9tYzAwMmczaXFrYzloNzlnYWoifQ.KAUJkm5WYkiL24-McUunig'
     request({url,json:true},(err,res)=>{
+        console.log(res.body.features)
         if(err){
             callback('Check your network conectivity',undefined)
-        }else if(res.body.features.length===0){
-            callback('Unable to find your query Try again!!!',undefined)
+        }else if(res.body.features.length==0){
+            callback('Unable to find your query Try More specific or check your query!!!',undefined)
         }else{
             callback(undefined,{
-                latitude:res.body.features[2].center[1],
-                langitude:res.body.features[2].center[0],
+                latitude:res.body.features[0].center[1],
+                langitude:res.body.features[0].center[0],
                 location:res.body.features[0].place_name
             })
         }
